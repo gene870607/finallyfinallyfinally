976aa0e235af5209bf07aa17b03bb3f5adabf1ca
俊佑 郭 <geneissmart@gmail.com>
2020-08-25 12:28:12 +0800

    first commit

---
+++ Storyteller/Controllers/ChatRoomController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Storyteller.Controllers
+{
+    public class ChatRoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ChartRoom(string id)
+        {
+            ViewBag.roomId = id;
+            return View();
+        }
+    }
+}
---
+++ Storyteller/Hubs/Chat.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storyteller.Hubs
+{
+    public class Chat : Hub
+    {
+
+        public async Task AddToGroup(string groupId,string myName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+
+            await Clients.Group(groupId).SendAsync("Send", $"{myName} 加入聊天.");
+        }
+
+        public Task SendMessage(string groupId, string userName, string message)
+        {
+            return Clients.Group(groupId).SendAsync("ReceiveGroupMessage", userName, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //連上線
+            await base.OnConnectedAsync();
+        }
+
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            //斷線
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
